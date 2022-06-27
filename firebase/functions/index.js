6a81b97be3ee97e9552efdb80a05c917532a17ae
Rossi0x <rwoodhurst@gmail.com>
2022-06-27 17:06:30 +0100

    fixed health api

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,10 +3,12 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
 admin.initializeApp();
 
 exports.addData = functions.https.onRequest(async (req, res) => {
     const data = req.body
+    data.timestamp = FieldValue.serverTimestamp()
     const writeData = await admin.firestore().collection('apple-health-data').add(data)
-    res.json({result: `Health Data added to db with ID: ${writeData.id}`})
+    res.json({result: `Health Data added to db: ${writeData.id}`})
 })
